99d677a47cd98860fb9dec6935ded51c0fb56f38
leavion <gameacc44@yandex.ru>
2025-03-27 16:43:08 +0500

    added requests for validations

--- gen/go/sso/sso_grpc.pb.go
+++ gen/go/sso/sso_grpc.pb.go
@@ -19,8 +19,10 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Auth_Register_FullMethodName = "/auth.Auth/Register"
-	Auth_Login_FullMethodName    = "/auth.Auth/Login"
+	Auth_Register_FullMethodName     = "/auth.Auth/Register"
+	Auth_Login_FullMethodName        = "/auth.Auth/Login"
+	Auth_IsUserExists_FullMethodName = "/auth.Auth/IsUserExists"
+	Auth_IsValidJWT_FullMethodName   = "/auth.Auth/IsValidJWT"
 )
 
 // AuthClient is the client API for Auth service.
@@ -29,6 +31,8 @@ const (
 type AuthClient interface {
 	Register(ctx context.Context, in *RegisterRequest, opts ...grpc.CallOption) (*RegisterResponse, error)
 	Login(ctx context.Context, in *LoginRequest, opts ...grpc.CallOption) (*LoginResponse, error)
+	IsUserExists(ctx context.Context, in *IsUserExistsRequest, opts ...grpc.CallOption) (*IsUserExistsResponse, error)
+	IsValidJWT(ctx context.Context, in *IsValidJWTRequest, opts ...grpc.CallOption) (*IsValidJWTResponse, error)
 }
 
 type authClient struct {
@@ -59,12 +63,34 @@ func (c *authClient) Login(ctx context.Context, in *LoginRequest, opts ...grpc.C
 	return out, nil
 }
 
+func (c *authClient) IsUserExists(ctx context.Context, in *IsUserExistsRequest, opts ...grpc.CallOption) (*IsUserExistsResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(IsUserExistsResponse)
+	err := c.cc.Invoke(ctx, Auth_IsUserExists_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *authClient) IsValidJWT(ctx context.Context, in *IsValidJWTRequest, opts ...grpc.CallOption) (*IsValidJWTResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(IsValidJWTResponse)
+	err := c.cc.Invoke(ctx, Auth_IsValidJWT_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServer is the server API for Auth service.
 // All implementations must embed UnimplementedAuthServer
 // for forward compatibility.
 type AuthServer interface {
 	Register(context.Context, *RegisterRequest) (*RegisterResponse, error)
 	Login(context.Context, *LoginRequest) (*LoginResponse, error)
+	IsUserExists(context.Context, *IsUserExistsRequest) (*IsUserExistsResponse, error)
+	IsValidJWT(context.Context, *IsValidJWTRequest) (*IsValidJWTResponse, error)
 	mustEmbedUnimplementedAuthServer()
 }
 
@@ -80,6 +106,12 @@ func (UnimplementedAuthServer) Register(context.Context, *RegisterRequest) (*Reg
 }
 func (UnimplementedAuthServer) Login(context.Context, *LoginRequest) (*LoginResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Login not implemented")
+}
+func (UnimplementedAuthServer) IsUserExists(context.Context, *IsUserExistsRequest) (*IsUserExistsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsUserExists not implemented")
+}
+func (UnimplementedAuthServer) IsValidJWT(context.Context, *IsValidJWTRequest) (*IsValidJWTResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method IsValidJWT not implemented")
 }
 func (UnimplementedAuthServer) mustEmbedUnimplementedAuthServer() {}
 func (UnimplementedAuthServer) testEmbeddedByValue()              {}
@@ -138,6 +170,42 @@ func _Auth_Login_Handler(srv interface{}, ctx context.Context, dec func(interfac
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Auth_IsUserExists_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IsUserExistsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).IsUserExists(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Auth_IsUserExists_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).IsUserExists(ctx, req.(*IsUserExistsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Auth_IsValidJWT_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IsValidJWTRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).IsValidJWT(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Auth_IsValidJWT_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).IsValidJWT(ctx, req.(*IsValidJWTRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Auth_ServiceDesc is the grpc.ServiceDesc for Auth service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -152,6 +220,14 @@ var Auth_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Login",
 			Handler:    _Auth_Login_Handler,
+		},
+		{
+			MethodName: "IsUserExists",
+			Handler:    _Auth_IsUserExists_Handler,
+		},
+		{
+			MethodName: "IsValidJWT",
+			Handler:    _Auth_IsValidJWT_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
